097df5e6d26f5c99dcc01592f181aaa16b9bf929
miyako <keisuke.miyako@4d.com>
2019-10-12 08:57:19 +0900

    remove build

--- zip/build/Zip.build/Deployment/Zip.build/Objects-normal/x86_64/4DPluginAPI.d
+++
@@ -1,7 +0,0 @@
-dependencies: \
-  /Users/miyako/miyako@github.com/4d-plugin-zip/zip/4D\ Plugin\ API/4DPluginAPI.c \
-  /Users/miyako/miyako@github.com/4d-plugin-zip/zip/4D\ Plugin\ API/4DPluginAPI.h \
-  /Users/miyako/miyako@github.com/4d-plugin-zip/zip/4D\ Plugin\ API/Flags.h \
-  /Users/miyako/miyako@github.com/4d-plugin-zip/zip/4D\ Plugin\ API/PublicTypes.h \
-  /Users/miyako/miyako@github.com/4d-plugin-zip/zip/4D\ Plugin\ API/PrivateTypes.h \
-  /Users/miyako/miyako@github.com/4d-plugin-zip/zip/4D\ Plugin\ API/EntryPoints.h
--- zip/build/Zip.build/Deployment/Zip.build/Objects-normal/x86_64/cpmap.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/miyako/miyako@github.com/4d-plugin-zip/zip/codepage/cpmap.cpp \
-  /Users/miyako/miyako@github.com/4d-plugin-zip/zip/codepage/cpmap.h
